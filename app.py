25ec3dbdcb0fbe28aa70de7862537560735b9323
veeringnugget <xxshaunataylor@gmail.com>
2025-06-19 14:57:58 +0100

    Added neccessary routes and active_pages

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ app = Flask(__name__)
 def index():
     return render_template('home.html', active_page='home')
 
+@app.route('/new_joke')
+def new_joke():
+    return render_template('new_joke.html', active_page='new_joke')
+
+@app.route('/sets')
+def sets():
+    return render_template('sets.html', active_page='build_set')
+
+@app.route('/archive')
+def archive():
+    return render_template('archive.html', active_page='archive')
+
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
