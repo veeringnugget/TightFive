ab55579bfa651f668e7208fb9c07f4f816a85875
veeringnugget <xxshaunataylor@gmail.com>
2025-06-29 18:11:50 +0100

    Joke API added & flash errors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
 import sqlite3
+import random
 import requests
 
 app = Flask(__name__)
@@ -8,10 +9,16 @@ app.secret_key = '9yZ)<2$lc`*pG>948{(ZrHk~]FpFSrk%M_<aeBDZ2~JyFV[6tk]ww!y%PB<X|3
 
 @app.route('/')
 def index():
+    # API for Joke Generator
     response = requests.get("https://icanhazdadjoke.com/", headers={"Accept": "application/json"})
     joke = response.json().get("joke")
-    print(joke)
-    return render_template('home.html', active_page='home', joke=joke)
+
+    # Output a random prompt
+    with open("resources/comedy_prompts.txt", "r") as file:
+        prompt = [line.strip() for line in file if line.strip()]
+    randomprompt = (random.choice(prompt))
+
+    return render_template('home.html', active_page='home', joke=joke, prompt=randomprompt)
 
 @app.route('/new_joke', methods=["GET", "POST"])
 def new_joke():
@@ -23,7 +30,21 @@ def new_joke():
         if not title:
             flash("Please enter a title", "title")
             error = True
-
+        # Tag
+        tag = request.form.get("tag")
+        if not tag:
+            flash("Please select a tag", "tag")
+            error = True
+        # Setup
+        setup = request.form.get("setup")
+        if not setup:
+            flash("Please enter a setup", "setup")
+            error = True
+        # Punchline
+        punchline = request.form.get("punchline")
+        if not punchline:
+            flash("Please enter a punchline", "punchline")
+            error = True
         return render_template('new_joke.html')
     with open("resources/tags.txt", "r") as file:
         tags = [line.strip() for line in file if line.strip()]
