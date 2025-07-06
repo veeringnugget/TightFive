56c8e416d68698724452cfd0bbc44ef5a8023e0e
veeringnugget <xxshaunataylor@gmail.com>
2025-07-06 14:38:51 +0100

    Created new_set.py to store the form when user creates a set

---
+++ routes/new_set.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request, flash
+import sqlite3
+
+new_set = Blueprint("new_set", __name__, static_folder="static", template_folder="templates")
+
+@new_set.route('/new_set')
+def new_set_form():
+    connect = sqlite3.connect("tightfive.db")
+    cursor = connect.cursor()
+    # Load current sets
+    sets = connect.execute("SELECT * FROM sets").fetchall()
+    if not sets:
+        sets = "No Sets Created"
+    connect.commit()
+    connect.close()
+    return render_template('new_set.html', active_page='sets', sets=sets)
