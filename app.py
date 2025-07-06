10241d3104da6773d1e5d4e94a5fc98a3fa2b4e3
veeringnugget <xxshaunataylor@gmail.com>
2025-07-06 14:38:31 +0100

    Added new_set blueprint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes.home import home
 from routes.new_material import new_material
 from routes.sets import sets
 from routes.vault import joke_vault
+from routes.new_set import new_set
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ app.register_blueprint(home)
 app.register_blueprint(new_material)
 app.register_blueprint(sets)
 app.register_blueprint(joke_vault)
+app.register_blueprint(new_set)
 
 app.secret_key = '9yZ)<2$lc`*pG>948{(ZrHk~]FpFSrk%M_<aeBDZ2~JyFV[6tk]ww!y%PB<X|3['
 
