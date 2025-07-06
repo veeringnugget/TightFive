78f476f21d5287557e89a6d204ec4d9a1b1ed90c
veeringnugget <xxshaunataylor@gmail.com>
2025-07-06 14:39:06 +0100

    Added SQL statement to get info from sets

--- routes/sets.py
+++ routes/sets.py
@@ -5,4 +5,12 @@ sets = Blueprint("sets", __name__, static_folder="static", template_folder="temp
 
 @sets.route('/sets')
 def new_set():
-    return render_template('sets.html', active_page='sets')
+    connect = sqlite3.connect("tightfive.db")
+    cursor = connect.cursor()
+    # Load current sets
+    sets = connect.execute("SELECT * FROM sets").fetchall()
+    if not sets:
+        sets = "No Sets Created"
+    connect.commit()
+    connect.close()
+    return render_template('sets.html', active_page='sets', sets=sets)
