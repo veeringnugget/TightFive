7e77cfa336ade7bdfa0aa6377cbf7b2d3a378186
veeringnugget <xxshaunataylor@gmail.com>
2025-07-21 07:14:08 +0100

    Get JSON data from JS

--- routes/new_set.py
+++ routes/new_set.py
@@ -1,20 +1,27 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, jsonify, make_response
 import sqlite3
 
 new_set = Blueprint("new_set", __name__, static_folder="static", template_folder="templates")
 
-@new_set.route('/new_set')
+@new_set.route('/new_set', methods=["GET", "POST"])
 def new_set_form():
     connect = sqlite3.connect("tightfive.db")
     cursor = connect.cursor()
+
     # Load current sets
-    sets = connect.execute("SELECT * FROM sets").fetchall()
+    sets = cursor.execute("SELECT * FROM sets").fetchall()
     if not sets:
         sets = "No Sets Created"
     # Load Jokes
-    jokes = connect.execute("SELECT * FROM jokes").fetchall()
+    jokes = cursor.execute("SELECT * FROM jokes").fetchall()
     if not jokes:
         jokes = "No Jokes Written"
+        
+    if request.method == "POST":
+            req = request.get_json()
+            print(req)
+            res = make_response(jsonify({"message": "JSON Recieived"}), 200)
+            return render_template('new_set.html', active_page='sets', sets=sets, jokes=jokes)
     connect.commit()
     connect.close()
     return render_template('new_set.html', active_page='sets', sets=sets, jokes=jokes)
