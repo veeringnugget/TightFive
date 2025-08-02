f095706c15852d2da9363987c03e4d7eccf3f4ee
veeringnugget <xxshaunataylor@gmail.com>
2025-08-02 20:11:38 +0100

    got POST request and used for SQL statement

--- routes/sets.py
+++ routes/sets.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, jsonify, make_response, url_for, redirect
 import sqlite3
 
 sets = Blueprint("sets", __name__, static_folder="static", template_folder="templates")
 
-@sets.route('/sets')
+@sets.route('/sets',methods=["GET", "POST"])
 def new_set():
     connect = sqlite3.connect("tightfive.db")
     cursor = connect.cursor()
@@ -11,6 +11,11 @@ def new_set():
     sets = connect.execute("SELECT * FROM sets").fetchall()
     if not sets:
         sets = "No Sets Created"
-    connect.commit()
-    connect.close()
+    # Load up SQL set information:
+    if request.method == "POST":
+        setName = request.get_json()
+        setId = cursor.execute("SELECT id FROM sets WHERE title = ?", (setName,)).fetchall()[0][0]
+        setTable = cursor.execute("SELECT id, title, description FROM sets WHERE id = ?", (setId,)).fetchone()
+        connect.close()
+        return jsonify({"setInfo": setTable)
     return render_template('sets.html', active_page='sets', sets=sets)
