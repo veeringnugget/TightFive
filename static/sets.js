888178d0d4a6dc38f21ea0cb119f55f4cc588547
veeringnugget <xxshaunataylor@gmail.com>
2025-08-08 08:10:01 +0100

    When set selected, display information about the individual set and create elements to fit this inside

--- static/sets.js
+++ static/sets.js
@@ -1,12 +1,27 @@
 // Global Variable:
 let activeEl
 
-
+// Functions:
 function removeBody(parentDiv){
     setBody = document.querySelector(".set-body")
     while (setBody.firstElementChild != null){
         setBody.firstElementChild.remove()
     }
+}
+
+function addNewRow(){
+    // Create new Row:
+    let setBody = document.querySelector(".set-body")
+    newRow = document.createElement("div")
+    newRow.classList.add("row", "mb-3")
+    setBody.appendChild(newRow)
+    return newRow
+}
+
+function newDiv(){
+    let newDiv = document.createElement("div")
+    newDiv.classList.add("col")
+    return newDiv
 }
 
 // When user selects a set, add a "selected" class to highlight
@@ -45,10 +60,42 @@ document.addEventListener("DOMContentLoaded", function(){
                 const desc = data.setInfo
                 document.querySelector(".set-desc").innerHTML = desc[2]
                 const sectionData = data.sectionsInfo
+                const jokesData = data.jokesInfo
                 for (let i = 0; i < sectionData.length; i++){
-                    newSection = document.createElement("newSection")
-                    newSection.innerHTML = "<p>Section " + sectionData[i][0] + "</p>"
-                    setBody.appendChild(newSection)
+                    // New Row:
+                    addNewRow()
+
+                    // Create new Section:
+                    let newSection = newDiv()
+                    newSection.classList.add("header")
+                    newSection.innerHTML = "Section " + sectionData[i][0]
+                    newRow.appendChild(newSection)
+
+                    // New Row:
+                    addNewRow()
+
+                    // Add Section Title Data:
+                    let sectionTitle = newDiv()
+                    sectionTitle.classList.add("col-sm-3", "subTitle")
+                    sectionTitle.innerHTML = "Section Title"
+                    newRow.appendChild(sectionTitle)
+                    let titleData = newDiv()
+                    titleData.innerHTML = sectionData[i][1]
+                    newRow.appendChild(titleData)
+
+                    // New Row:
+                    addNewRow()
+
+                    // Add Section Length Data:
+                    let sectionLength = newDiv()
+                    sectionLength.classList.add("col-sm-3", "subTitle")
+                    sectionLength.innerHTML = "Section Length"
+                    newRow.appendChild(sectionLength)
+                    let lengthData = newDiv()
+                    lengthData.innerHTML = sectionData[i][2] + " minutes"
+                    newRow.appendChild(lengthData)
+                    // Loop through and add the jokes in order
+                    console.log(data.jokesData)
                 }
             })
         }
