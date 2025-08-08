36924276081c7bfddc733d55bec2261754440e6f
veeringnugget <xxshaunataylor@gmail.com>
2025-08-08 08:16:10 +0100

    Execute SQL select statements to get jokes in order of sectional / joke

--- routes/sets.py
+++ routes/sets.py
@@ -16,6 +16,8 @@ def new_set():
         setName = request.get_json()
         setId = cursor.execute("SELECT id FROM sets WHERE title = ?", (setName,)).fetchall()[0][0]
         setTable = cursor.execute("SELECT id, title, description FROM sets WHERE id = ?", (setId,)).fetchone()
+        sectionsTable = cursor.execute("SELECT section_id, section_name, LENGTH FROM sections WHERE set_id = ?", (setId,)).fetchall()
+        sectionJokes = cursor.execute("SELECT sectional_jokes.section_id, jokes.title, sectional_jokes.order_index FROM sectional_jokes JOIN jokes ON sectional_jokes.joke_id=jokes.id WHERE sectional_jokes.set_id = ? ORDER BY sectional_jokes.section_id, sectional_jokes.order_index", (setId,)).fetchall()
         connect.close()
-        return jsonify({"setInfo": setTable})
+        return jsonify({"setInfo": setTable, "sectionsInfo": sectionsTable, "jokesData": sectionJokes})
     return render_template('sets.html', active_page='sets', sets=sets)
